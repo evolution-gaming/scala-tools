9ca72bf0fc5e8aca66920bb7d38ab5946572d024
Yaroslav Klymko <t3hnar@gmail.com>
2016-10-29 13:10:14 +0300

    test more cases in FutureEitherSpec

--- src/test/scala/com/evolutiongaming/util/FutureEitherSpec.scala
+++ src/test/scala/com/evolutiongaming/util/FutureEitherSpec.scala
@@ -1,8 +1,7 @@
 package com.evolutiongaming.util
 
-import org.scalatest.{FunSuite, Matchers}
-
 import com.evolutiongaming.util.Validation._
+import org.scalatest.{FunSuite, Matchers}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -18,18 +17,59 @@ class FutureEitherSpec extends FunSuite with Matchers {
   }
 
   test("flatMap") {
-    val fe = FutureEither("right") flatMap {_ => sys error "error"}
+    val fe = FutureEither("right") flatMap { _ => sys error "error" }
     intercept[RuntimeException] {
       await(fe.future)
     }
   }
 
   test("map") {
-    val fe = FutureEither("right") map {_ => sys error "error"}
+    val fe = FutureEither("right") map { _ => sys error "error" }
     intercept[RuntimeException] {
       await(fe.future)
     }
   }
 
-  def await[T](awaitable: Awaitable[T]) = Await.result(awaitable, 10.seconds)
+  test("result") {
+    FutureEither("right") result timeout shouldEqual "right".ok
+    FutureEither("left".ko) result timeout shouldEqual "left".ko
+    FutureEither(Future successful "right".ok) result timeout shouldEqual "right".ok
+  }
+
+  test("leftMap") {
+    FutureEither[String, String]("right") leftMap { _ => 0 } result timeout shouldEqual "right".ok
+    FutureEither[String, String]("left".ko) leftMap { _ => 0 } result timeout shouldEqual 0.ko
+    FutureEither[String, String](Future successful "right".ok) leftMap { _ => 0 } result timeout shouldEqual "right".ok
+    FutureEither[String, String](Future successful "left".ko) leftMap { _ => 0 } result timeout shouldEqual 0.ko
+  }
+
+  test("fold") {
+    await(FutureEither[String, String]("right").fold(_ => 1, _ => 2)) shouldEqual 2
+    await(FutureEither[String, String]("left".ko).fold(_ => 1, _ => 2)) shouldEqual 1
+    await(FutureEither[String, String](Future successful "right".ok).fold(_ => 1, _ => 2)) shouldEqual 2
+    await(FutureEither[String, String](Future successful "left".ko).fold(_ => 1, _ => 2)) shouldEqual 1
+  }
+
+  test("getOrElse") {
+    await(FutureEither[String, String]("right") getOrElse { _ => "l" }) shouldEqual "right"
+    await(FutureEither[String, String]("left".ko) getOrElse { _ => "l" }) shouldEqual "l"
+    await(FutureEither[String, String](Future successful "right".ok) getOrElse { _ => "l" }) shouldEqual "right"
+    await(FutureEither[String, String](Future successful "left".ko) getOrElse { _ => "l" }) shouldEqual "l"
+  }
+
+  test("either") {
+    await(FutureEither[String, String]("right").either) shouldEqual "right".ok
+    await(FutureEither[String, String]("left".ko).either) shouldEqual "left".ko
+    await(FutureEither[String, String](Future successful "right".ok).either) shouldEqual "right".ok
+    await(FutureEither[String, String](Future successful "left".ko).either) shouldEqual "left".ko
+  }
+
+  test("recover") {
+    val fe = FutureEither[String, String](Future.failed(new RuntimeException)) recover { case _: RuntimeException => "left".ko }
+    fe.result(timeout) shouldEqual "left".ko
+  }
+
+  val timeout = 10.seconds
+
+  def await[T](awaitable: Awaitable[T]) = Await.result(awaitable, timeout)
 }
