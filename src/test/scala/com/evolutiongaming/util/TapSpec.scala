df72d8ba7cd1cfd9e5f2c294901a4ee7e23b4491
Oleg Kovalenko <okovalenko@evolutiongaming.com>
2019-03-25 20:30:23 +0200

    add let, |>

--- src/test/scala/com/evolutiongaming/util/TapSpec.scala
+++ src/test/scala/com/evolutiongaming/util/TapSpec.scala
@@ -4,11 +4,18 @@ import com.evolutiongaming.util.Tap._
 import org.scalatest.{FunSuite, Matchers}
 
 class TapSpec extends FunSuite with Matchers {
-  test("apply function to itself") {
+  test("tap") {
     0 tap { _ shouldEqual 0 }
+    0 tap { _ => } shouldEqual 0
   }
 
-  test("returns itself after function application") {
-    0 tap { _ => } shouldEqual 0
+  test("let") {
+    0 let { _ shouldEqual 0 }
+    0 let { _ + 1 } shouldEqual 1
+  }
+
+  test("|>") {
+    0 |> { _ shouldEqual 0 }
+    0 |> { _ + 1 } shouldEqual 1
   }
 }
